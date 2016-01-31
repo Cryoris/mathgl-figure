419307b6cff377574973745b23e2beda01b25f70
Julien Gacon <jgacon@student.ethz.ch>
2016-01-31 22:53:28 +0100

    added another example plot

--- examples/6/multiple.cpp
+++ examples/6/multiple.cpp
@@ -12,6 +12,19 @@ int main () {
   }
   fig.save("many.eps");
 
+  mgl::Figure fig2;
+  fig2.ranges(0, 10, 0, 16);
+  fig2.fplot("0.2*x", "b0");
+  fig2.fplot("0.4*x", "b1");
+  fig2.fplot("0.6*x", "b2");
+  fig2.fplot("0.8*x", "b3");
+  fig2.fplot("x", "b4");
+  fig2.fplot("1.2*x", "b5");
+  fig2.fplot("1.4*x", "b6");
+  fig2.fplot("1.6*x", "b7");
+  fig2.save("manysame.eps");
+  
+
   return 0;
 }
 
