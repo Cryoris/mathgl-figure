341e965db52e8d63955aee34104cb29b0ff0c92e
Julien Gacon <jgacon@student.ethz.ch>
2016-02-19 11:26:39 +0100

    added find libFigure.a
    - looking for libFigure.a in default paths
    - setting FIGURE_LIBRARY to path/to/library/libFigure.a if found

--- modules/FindFigure.cmake
+++ modules/FindFigure.cmake
@@ -1,6 +1,7 @@
 # ================================= FindFigure.cmake =======================================
 # once finshed the following variables will be initialized:
 #   FIGURE_INCLUDE_DIR : directory which contains all Figure files
+#   FIGURE_LIBRARY : libFigure.a
 # ==========================================================================================
 #   Typical usage:    find_package( Figure REQUIRED)
 #                     include_directories( ${FIGURE_INCLUDE_DIR} )
@@ -11,11 +12,11 @@ if ( DEBUG )
   message( STATUS "Running FindFigure.cmake with DEBUG option .." )
 endif()
 
+
+## ----------------- find header files ------------------ ##
 # get paths to figure files
 set( FIGURE_PATH_SUFFIX figure )
 find_path( FIGURE_HPP NAMES figure.hpp PATH_SUFFIXES ${FIGURE_PATH_SUFFIX} DOC "Figure header" )
-find_path( FIGURE_CPP NAMES figure.hpp PATH_SUFFIXES ${FIGURE_PATH_SUFFIX} DOC "Figure source" )
-find_path( FIGURE NAMES Figure PATH_SUFFIXES ${FIGURE_PATH_SUFFIX} DOC "Figure include" )
 find_path( FIGURECONFIG_HPP NAMES FigureConfig.hpp PATH_SUFFIXES ${FIGURE_PATH_SUFFIX} DOC "Figure config" )
 
 find_path( MGL_LABEL_HPP NAMES MglLabel.hpp PATH_SUFFIXES ${FIGURE_PATH_SUFFIX} DOC "MglLabel" )
@@ -23,8 +24,6 @@ find_path( MGL_PLOT_HPP NAMES MglPlot.hpp PATH_SUFFIXES ${FIGURE_PATH_SUFFIX} DO
 find_path( MGL_STYLE_HPP NAMES MglStyle.hpp PATH_SUFFIXES ${FIGURE_PATH_SUFFIX} DOC "MglStyle" )
 
 set( FIGURE_PATHS ${FIGURE_HPP} 
-                  ${FIGURE_CPP}
-                  ${FIGURE}
                   ${FIGURECONFIG_HPP}
                   ${MGL_LABEL_HPP}
                   ${MGL_PLOT_HPP}
@@ -32,13 +31,13 @@ set( FIGURE_PATHS ${FIGURE_HPP}
                   )
 
 if ( DEBUG )
-  message( STATUS "Needed files are: figure.hpp figure.cpp Figure FigureConfig.hpp MglLabel.hpp MglPlot.hpp MglStyle.hpp" )
+  message( STATUS "Needed files are: figure.hpp FigureConfig.hpp MglLabel.hpp MglPlot.hpp MglStyle.hpp" )
 endif()
 
 # check if the files are all in the correct place
 set( PATHS_ARE_VALID true )
 foreach( FILE_PATH ${FIGURE_PATHS} )
-  if( NOT ${FILE_PATH} STREQUAL ${FIGURE} ) 
+  if( NOT ${FILE_PATH} STREQUAL ${FIGURE_HPP} ) 
     set( PATHS_ARE_VALID false )
 
     if ( DEBUG ) # this will print which file exactly wasnt found
@@ -50,14 +49,27 @@ endforeach()
 
 # if all filepaths are valid set FIGURE_INCLUDE_DIR, otherwise throw error/warning
 if( PATHS_ARE_VALID )
-  message( STATUS "Found Figure in ${FIGURE}" )
-  set( FIGURE_INCLUDE_DIR ${FIGURE} )
+  message( STATUS "Found Figure in ${FIGURE_HPP}" )
+  set( FIGURE_INCLUDE_DIR ${FIGURE_HPP} )
 else()
   if( Figure_REQUIRED )
-    message(FATAL "Couldn't find all necessary files for Figure, maybe try (re-)installing with administrator rights?")
-    message("   Run cmake with the '-DDEBUG=1' option for more information on which files are missing.")
+    message( FATAL_ERROR "Couldn't find all necessary files for Figure, maybe try (re-)installing with administrator rights?" )
+    message( "   Run cmake with the '-DDEBUG=1' option for more information on which files are missing." )
   else()
-    message(STATUS "Couldn't find all necessary files for Figure, maybe try (re-)installing with administrator rights?")
-    message("   Run cmake with the '-DDEBUG=1' option for more information on which files are missing.")
+    message( STATUS "Couldn't find all necessary files for Figure, maybe try (re-)installing with administrator rights?" )
+    message( "   Run cmake with the '-DDEBUG=1' option for more information on which files are missing." )
+  endif()
+endif()
+
+## ----------------- find libFigure.a ------------------- ##
+find_library( FIGURE_LIBRARY NAMES libFigure.a DOC "Figure library" )
+if( FIGURE_LIBRARY )
+  get_filename_component( FIGURE_LIBRARY_DIR ${FIGURE_LIBRARY} PATH )
+  message( STATUS "Found Figure library: ${FIGURE_LIBRARY_DIR}" )
+else()
+  if( Figure_REQUIRED )
+    message( FATAL_ERROR "Couldn't find libFigure.a, maybe try (re-)installing with administrator rights?" )
+  else()
+    message( STATUS "Couldn't find libFigure.a, maybe try (re-)installing with administrator rights?" )
   endif()
 endif()
