ebc7b6f8e34f4b1050532c0fc3f6c6b758b04b6d
Julien Gacon <jgacon@student.ethz.ch>
2016-02-25 19:34:57 +0100

    more and more consistent comments

--- src/figure.hpp
+++ src/figure.hpp
@@ -20,10 +20,9 @@
 
 namespace mgl {
 
-//!
-/*!
- * Make mglData from std::vector
- */
+/* make mglData from std::vector                                    *
+ * PRE: -                                                           *
+ * POST: returning mglData containing the data of given std::vector */
 template<typename Scalar>
 typename std::enable_if<std::is_arithmetic<Scalar>::value, mglData>::type
 make_mgldata(const std::vector<Scalar>& v) {
@@ -31,10 +30,9 @@ make_mgldata(const std::vector<Scalar>& v) {
   return mglData(vd.data(), vd.size());
 }
 
-//!
-/*!
- * Make mglData from Eigen::Vector or Eigen::RowVector
- */
+/* make mglData from Eigen::Vector or Eigen::RowVector                     *
+ * PRE : -                                                                 *
+ * POST: returning mglData containing the data of given Eigen::(Row)Vector */
 # if FIG_HAS_EIGEN
 template<typename Derived>
 mglData make_mgldata(const Eigen::MatrixBase<Derived>& vec) {
@@ -106,45 +104,52 @@ private:
   std::array<double, 4> ranges_; // axis ranges
   std::array<double, 2> zranges_; // z axis ranges
   bool autoRanges_; // auto ranges or ranges as the user set them?
-  std::string title_;
-  std::string xFunc_, yFunc_, zFunc_;
-  MglLabel xMglLabel_, yMglLabel_;
-  MglStyle styles_;
+  std::string title_; // title of the plot
+  std::string xFunc_, yFunc_, zFunc_; // curvature of coordinate axis
+  MglLabel xMglLabel_, yMglLabel_; // x and y labels of the plot
+  MglStyle styles_; // styles of the plots
   double fontSizePT_; // font size in PT
-  int figHeight_, figWidth_;
-  std::vector<std::unique_ptr<MglPlot> > plots_;
+  int figHeight_, figWidth_; // height and width of the graphic
+  std::vector<std::unique_ptr<MglPlot> > plots_; // x, y (and z) data for the plots
 };
 
-/* plot y data
- * PRE : -
- * POST: add (range(1, length(y)))-y to plot queue with given style (must be given!) and set legend (optional) */
+/* plot y data                                                         *
+ * PRE : -                                                             *
+ * POST: add [1:length(y)]-y to plot queue with given style (optional) */
 template <typename yVector>
 MglPlot& Figure::plot(const yVector& y, std::string style)
 {
+  // build a fitting x vector for the y vector
   std::vector<double> x(y.size());
   std::iota(x.begin(), x.end(), 1);
   return plot(x, y, style);
 }
 
-/* plot x,y data
- * PRE : -
- * POST: add x-y to plot queue with given style (must be given!) and set legend (optional) */
-// same syntax for Eigen::VectorXd and std::vector<T>
+/* plot x,y data                                           *
+ * PRE : -                                                 *
+ * POST: add x-y to plot queue with given style (optional) */
 template <typename xVector, typename yVector>
+// the long template magic expression ensures this function is not called if yVector is a string (which would be allowed as it is a templated argument)
 typename std::enable_if<!std::is_same<typename std::remove_pointer<typename std::decay<yVector>::type>::type, char >::value, MglPlot&>::type
 Figure::plot(const xVector& x, const yVector& y, std::string style)
 {
+  // make sure the sizes of the vectors are the same
   if (x.size() != y.size()){
     std::cerr << "In function Figure::plot(): Vectors must have same sizes!";
   }
 
+  // build mglData from the x and y vectors
   mglData xd = make_mgldata(x);
   mglData yd = make_mgldata(y);
 
+  // if the ranges are set to auto set the new ranges 
   if(autoRanges_){
-    setRanges(xd, yd, 0.);
+    setRanges(xd, yd, 0.); // the 0 stands no top+bottom margin
   }
   
+  // check if a style is given,
+  // if yes: add that style to the style queue and remove it from the style-deque,
+  // if no : get a new style from the style-deque
   if (style.size() == 0) {
     style = styles_.get_next();
   }
@@ -152,28 +157,38 @@ Figure::plot(const xVector& x, const yVector& y, std::string style)
     styles_.eliminate(style);
   }
 
+  // put the x-y data in the plot queue
   plots_.emplace_back(std::unique_ptr<MglPlot2d>(new MglPlot2d(xd, yd, style)));
   return *plots_.back().get();
 }
 
+/* plot x,y,z data                                           *
+ * PRE : -                                                   *
+ * POST: add x-y-z tp plot queue with given style (optional) */
 template <typename xVector, typename yVector, typename zVector>
 MglPlot& Figure::plot3(const xVector& x, const yVector& y, const zVector& z, std::string style)
 {
 
   has_3d_ = true; // needed to set zranges in save-function and call mgl::Rotate
 
+  // make sure the sizes of the vectors are the same
   if (!(x.size() == y.size() && y.size() == z.size())){
     std::cerr << "In function Figure::plot(): Vectors must have same sizes!";
   }
 
+  // build mglData from x, y and z vectors
   mglData xd = make_mgldata(x);
   mglData yd = make_mgldata(y);
   mglData zd = make_mgldata(z);
 
+  // if the ranges are set to auto set the new ranges
   if(autoRanges_){
     setRanges(xd, yd, zd);
   }
 
+  // check if a style is given,
+  // if yes: add that style to the style queue and remove it from the style-deque,
+  // if no : get a new style from the style-deque
   if (style.size() == 0) {
     style = styles_.get_next();
   }
@@ -181,6 +196,7 @@ MglPlot& Figure::plot3(const xVector& x, const yVector& y, const zVector& z, std
     styles_.eliminate(style);
   }
 
+  // put the x-y-z data in the plot queue
   plots_.emplace_back(std::unique_ptr<MglPlot3d>(new MglPlot3d(xd, yd, zd, style)));
   return *plots_.back().get();
 }
