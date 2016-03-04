f57eddda5a333c350ccd9e7b73abb179548408e2
Julien Gacon <jgacon@student.ethz.ch>
2016-03-04 09:03:05 +0100

    legend plots for documentation

--- doc/examples_from_doc/legend/legend.cpp
+++ doc/examples_from_doc/legend/legend.cpp
@@ -6,10 +6,12 @@ int main() {
   prepare_data(x, y, 4);
 
   mgl::Figure f,g;
+  f.title("Default legend");
   f.plot(x, y).label("My Function");
   f.legend();
   f.save("legend_1");
 
+  g.title("Manual positioning");
   g.plot(x, y).label("My Function");
   g.legend(0.5, 0.25);
   g.save("legend_2");
